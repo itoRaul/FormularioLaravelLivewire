e7e9da14d5a2660ba3d32d6753628bd0c49c1372
itoRaul <raulferrito@gmail.com>
2025-09-15 14:38:32 -0500

    feat: adição de novas rotas

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Forms;
 
-Route::get('form', Forms::class);
+
+Route::get('form/{id?}', Forms::class)->name('form.edit');
 
 Route::get('/', function () {
     return view('welcome');
