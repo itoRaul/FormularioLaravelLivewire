a03152d60843da373d1fbc81d3cc22e86aff1373
itoRaul <raulferrito@gmail.com>
2025-09-15 03:11:32 -0400

    feat: criação da migration dados

---
+++ database/migrations/2025_09_15_063552_create_dados_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomecompleto');
+            $table->string('nomesocial')->nullable();
+            $table->string('cpf')->unique();
+            $table->string('rg')->unique();
+            $table->string('email');
+            $table->string('telefone');
+            $table->string('cep');
+            $table->string('logradouro');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('complemento')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dados');
+    }
+};
